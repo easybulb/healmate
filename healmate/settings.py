ee759c9f34385b1e168b5ae8a7d1fe9433a6f75f
Henry Ofodieze <easybulb@gmail.com>
2024-09-20 23:09:04 +0000

    Edit env file with the right secret key

--- healmate/settings.py
+++ healmate/settings.py
@@ -29,7 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+if os.path.isfile("env.py"):
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['8000-easybulb-healmate-8b8jtucddwa.ws.codeinstitute-ide.net','.herokuapp.com']
 CSRF_TRUSTED_ORIGINS = ['https://*.codeinstitute-ide.net', 'https://*.herokuapp.com']
