92b151551a95d60d3d3744c3e58b0f493fc19aad
Henry Ofodieze <easybulb@gmail.com>
2024-09-21 06:59:51 +0000

    fix urls and template setup

--- healmate/urls.py
+++ healmate/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from accounts.views import HomePage
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomePage.as_view(), name='home'),
+    path('', include('accounts.urls'), name='home'),
 ]
