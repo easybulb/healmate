b8673fbc7aff9b5f12e9ffbda6af178de1b17479
Henry Ofodieze <easybulb@gmail.com>
2024-09-21 06:08:06 +0000

    modify TEMPLATES array to include TEMPLATES_DIR in settings.py

--- healmate/settings.py
+++ healmate/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'healmate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
