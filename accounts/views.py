ef90ce64bdd4152aebaae794a48e0a50241c4293
Henry Ofodieze <easybulb@gmail.com>
2024-09-21 06:25:45 +0000

    update views.py to render index.html

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+
+class HomePage(TemplateView):
+    """
+    Displays the homepage
+    """
+    template_name = 'index.html'
