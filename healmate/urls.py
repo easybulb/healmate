858f6c86ff485febac5db338f8a4ea45b52c70a6
Henry Ofodieze <easybulb@gmail.com>
2024-09-23 06:46:01 +0000

    add error handlers for 404 and 500 errors in urls.py

--- healmate/urls.py
+++ healmate/urls.py
@@ -16,6 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404, handler500
+from django.shortcuts import render
+
+def custom_404(request, exception):
+    return render(request, 'core/404.html', status=404)
+
+def custom_500(request):
+    return render(request, 'core/500.html', status=500)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
