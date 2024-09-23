aae17a77c194738eeb7f9e26f45613df22be9a55
Henry Ofodieze <easybulb@gmail.com>
2024-09-23 07:12:41 +0000

    add class-based views in dashboard views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class PatientDashboardView(TemplateView):
+    template_name = 'dashboard/patient_dashboard.html'
+
+class SpecialistDashboardView(TemplateView):
+    template_name = 'dashboard/specialist_dashboard.html'
+
+class AdminDashboardView(TemplateView):
+    template_name = 'dashboard/admin_dashboard.html'
