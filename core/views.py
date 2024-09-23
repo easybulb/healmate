54e43fa79bf2d889e3970a5d8b5e0ca23b3828ba
Henry Ofodieze <easybulb@gmail.com>
2024-09-23 00:18:07 +0000

    add views for static pages

--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class HomePage(TemplateView):
+    template_name = 'core/home.html'
+
+class AboutPage(TemplateView):
+    template_name = 'core/about.html'
+
+class ContactPage(TemplateView):
+    template_name = 'core/contact.html'
+
+class JoinUsPage(TemplateView):
+    template_name = 'core/join_us.html'
+
+class ToolsPage(TemplateView):
+    template_name = 'core/tools.html'
+
+class FAQPage(TemplateView):
+    template_name = 'core/faq.html'
