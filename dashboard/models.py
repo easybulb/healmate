8e69cac91fbd9a052570124136c649fbfd140773
Henry Ofodieze <easybulb@gmail.com>
2024-09-24 02:45:18 +0000

    Add PatientProfile model & field to dashboard model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class PatientProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    contact_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.user.username}'s Patient Profile"
+
