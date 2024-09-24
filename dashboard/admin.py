bfcb0585864480af0f825654bd950d0d6d641ed6
Henry Ofodieze <easybulb@gmail.com>
2024-09-24 03:05:18 +0000

    register all models in dashboard admin.py

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import PatientProfile, SpecialistProfile, Appointment, Message, Feedback, Specialty
 
 # Register your models here.
+admin.site.register(PatientProfile)
+admin.site.register(SpecialistProfile)
+admin.site.register(Appointment)
+admin.site.register(Message)
+admin.site.register(Feedback)
+admin.site.register(Specialty)
