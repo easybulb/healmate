ceb13da99cd6ca7d9d8869dc043d1106261985ee
Henry Ofodieze <easybulb@gmail.com>
2024-09-24 07:09:58 +0000

    revert to previous dashboard views.py for bug test

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,34 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.contrib.auth.decorators import login_required, user_passes_test
-from django.utils.decorators import method_decorator
-
-# Create your views here.
-# Function to check if the user is a patient
-def is_patient(user):
-    return user.groups.filter(name='Patient').exists()
 
 
-# Function to check if the user is a specialist
-def is_specialist(user):
-    return user.groups.filter(name='Specialist').exists()
-
-
-# Function to check if the user is an admin
-def is_admin(user):
-    return user.groups.filter(name='Admin').exists()
-
-
-
-
-@method_decorator([login_required, user_passes_test(is_patient)], name='dispatch')
 class PatientDashboardView(TemplateView):
     template_name = 'dashboard/patient_dashboard.html'
 
-@method_decorator([login_required, user_passes_test(is_specialist)], name='dispatch')
 class SpecialistDashboardView(TemplateView):
     template_name = 'dashboard/specialist_dashboard.html'
 
-@method_decorator([login_required, user_passes_test(is_admin)], name='dispatch')
 class AdminDashboardView(TemplateView):
     template_name = 'dashboard/admin_dashboard.html'
--- healmate/settings.py
+++ healmate/settings.py
@@ -24,6 +24,7 @@ else:
     DEBUG = False
 
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
