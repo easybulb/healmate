2292ec097ddfd9f9b404ee23bb2df57443868bae
Henry Ofodieze <easybulb@gmail.com>
2024-09-24 02:50:05 +0000

    Add SpecialistProfile model to dashboard model.py

--- dashboard/models.py
+++ dashboard/models.py
@@ -8,3 +8,17 @@ class PatientProfile(models.Model):
     def __str__(self):
         return f"{self.user.username}'s Patient Profile"
 
+class Specialty(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class SpecialistProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    specialty = models.ForeignKey(Specialty, on_delete=models.SET_NULL, null=True)
+    bio = models.TextField()
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.specialty.name}"
