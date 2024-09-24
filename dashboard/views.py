9bf96fae3f9b9aa09b83f5d43a8eb7ccb9af0e1a
Henry Ofodieze <easybulb@gmail.com>
2024-09-24 14:30:51 +0000

    revert dashboard views.py tp original after testing

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,31 +2,18 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.utils.decorators import method_decorator
-from django.core.exceptions import PermissionDenied
 
 # Function to check if the user is a patient
 def is_patient(user):
-    if not user.groups.filter(name='Patient').exists():
-        print("User is not in the 'Patient' group")
-        raise PermissionDenied  # Raise a 403 Forbidden error
-    print("User is in the 'Patient' group")
-    return True
+    return user.groups.filter(name='Patient').exists()
 
 # Function to check if the user is a specialist
 def is_specialist(user):
-    if not user.groups.filter(name='Specialist').exists():
-        print("User is not in the 'Specialist' group")
-        raise PermissionDenied
-    print("User is in the 'Specialist' group")
-    return True
+    return user.groups.filter(name='Specialist').exists()
 
 # Function to check if the user is an admin
 def is_admin(user):
-    if not user.groups.filter(name='Admin').exists():
-        print("User is not in the 'Admin' group")
-        raise PermissionDenied
-    print("User is in the 'Admin' group")
-    return True
+    return user.groups.filter(name='Admin').exists()
 
 # Patient Dashboard View
 @method_decorator([login_required, user_passes_test(is_patient)], name='dispatch')
