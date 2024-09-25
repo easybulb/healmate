9fbc69f3c0e3a88250e725522b906d0c2e861f6e
Henry Ofodieze <easybulb@gmail.com>
2024-09-25 20:04:18 +0000

    fix accounts signal.py to use the correct group

--- accounts/signals.py
+++ accounts/signals.py
@@ -6,7 +6,6 @@ from dashboard.models import PatientProfile
 @receiver(post_save, sender=User)
 def add_to_patients_group_and_create_profile(sender, instance, created, **kwargs):
     if created:
-        patients_group, _ = Group.objects.get_or_create(name='Patients')
+        patients_group, _ = Group.objects.get_or_create(name='Patient')
         instance.groups.add(patients_group)
         PatientProfile.objects.get_or_create(user=instance)
-        
