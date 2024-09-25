9cc4575cd3fe64bfb4c46636ac903e2b37136739
Henry Ofodieze <easybulb@gmail.com>
2024-09-25 17:58:38 +0000

    add additional fields to PatientProfileForm

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -4,4 +4,4 @@ from dashboard.models import PatientProfile
 class PatientProfileForm(forms.ModelForm):
     class Meta:
         model = PatientProfile
-        fields = ['contact_number']  # More fields if necessary
+        fields = ['contact_number' 'address', 'date_of_birth', 'gender', 'medical_history', 'emergency_contact']  # More fields if necessary
