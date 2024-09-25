2e5c9b13a052cfb283d33d5b2aef78212d2b30ea
Henry Ofodieze <easybulb@gmail.com>
2024-09-25 17:27:14 +0000

    bug fix where signal didnt work for patient creation at registration

--- accounts/signals.py
+++ accounts/signals.py
@@ -6,9 +6,7 @@ from dashboard.models import PatientProfile
 @receiver(post_save, sender=User)
 def add_to_patients_group_and_create_profile(sender, instance, created, **kwargs):
     if created:
-        # Add the user to the 'Patients' group
         patients_group, _ = Group.objects.get_or_create(name='Patients')
         instance.groups.add(patients_group)
-
-        # Create a PatientProfile for the user
         PatientProfile.objects.get_or_create(user=instance)
+        
