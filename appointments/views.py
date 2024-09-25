610573c1b7785f60cec290e4760edfa6924b191e
Henry Ofodieze <easybulb@gmail.com>
2024-09-25 15:09:24 +0000

    update appointments/view.py to handle appointments

--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from dashboard.models import SpecialistProfile
+from .forms import AppointmentForm
 
 # Create your views here.
+
+def book_appointment(request, specialist_id):
+    specialist = get_object_or_404(SpecialistProfile, id=specialist_id)
+    if request.method == 'POST':
+        form = AppointmentForm(request.POST)
+        if form.is_valid():
+            appointment = form.save(commit=False)
+            appointment.specialist = specialist
+            appointment.patient = request.user.patientprofile  # Ensure the user is a patient
+            appointment.status = 'Scheduled'  # Set the default status to 'Scheduled'
+            appointment.save()
+            return redirect('appointment_confirmation')
+    else:
+        form = AppointmentForm()
+    return render(request, 'appointments/book_appointment.html', {'form': form, 'specialist': specialist})
+
