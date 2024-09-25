eb7433c47d2de7cf73860a7c3b4711ba624a79b9
Henry Ofodieze <easybulb@gmail.com>
2024-09-25 07:54:10 +0000

    fix bug: search form not showing on home page. check bugs in issues

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePage.as_view(), name='home'),
+    path('', views.home, name='home'),
     path('about/', views.AboutPage.as_view(), name='about'),
     path('contact-us/', views.ContactPage.as_view(), name='contact_us'),
     path('join-us/', views.JoinUsPage.as_view(), name='join_us'),
