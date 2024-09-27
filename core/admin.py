7bff0b15bdcbffdf011fe0bebd88b66fac57d8f9
Henry Ofodieze <easybulb@gmail.com>
2024-09-27 05:29:27 +0000

    fix load error by deleting error code of import PatientProfile

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import PatientProfile
 
 # Register your models here.
-@admin.register(PatientProfile)
-class PatientProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'is_active')  # Shows the user and their active status
-    list_filter = ('is_active',)  # Allows filtering by active/inactive status
