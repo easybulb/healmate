a8ad07fddd8ce021ec555cae3ea52a940caab81b
Henry Ofodieze <easybulb@gmail.com>
2024-09-27 15:49:25 +0000

    modify middleware.py logout user if account is inactive

--- dashboard/middleware.py
+++ dashboard/middleware.py
@@ -1,12 +1,18 @@
 from django.shortcuts import redirect
 from django.contrib import messages
+from django.conf import settings
 
 class InactiveUserMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.user.is_authenticated and not request.user.is_active:
-            messages.error(request, 'Your account has been deactivated. Please contact support for assistance.')
-            return redirect('account_logout')  # Log out inactive users
-        return self.get_response(request)
+        if request.user.is_authenticated:
+            if not request.user.is_active:
+                # Log out the user if their account is inactive
+                from django.contrib.auth import logout
+                logout(request)
+                messages.error(request, "Your account has been deactivated. Please contact admin for reactivation.")
+                return redirect('account_login')  # Redirect to login page
+        response = self.get_response(request)
+        return response
