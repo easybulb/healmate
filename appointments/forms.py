f9489e58688870df9aa35e09f71bcd679882a47b
Henry Ofodieze <easybulb@gmail.com>
2024-09-28 08:07:55 +0000

    create AvailabilityForm to allow specialists to set availability dates and time

--- appointments/forms.py
+++ appointments/forms.py
@@ -10,3 +10,14 @@ class AppointmentForm(forms.ModelForm):
             'date': forms.DateInput(attrs={'type': 'date'}),
             'time': forms.TimeInput(attrs={'type': 'time'}),
         }
+
+
+class AvailabilityForm(forms.ModelForm):
+    class Meta:
+        model = Availability
+        fields = ['date', 'start_time', 'end_time']
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'}),
+            'start_time': forms.TimeInput(attrs={'type': 'time'}),
+            'end_time': forms.TimeInput(attrs={'type': 'time'}),
+        }
