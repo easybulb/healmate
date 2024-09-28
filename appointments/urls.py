613f4f7e9754624590960ac7278f425c3ba76ef0
Henry Ofodieze <easybulb@gmail.com>
2024-09-28 02:18:41 +0000

    update apptmt. views.py with patient and specialist appointments url paths

--- appointments/urls.py
+++ appointments/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('book/<int:specialist_id>/', views.book_appointment, name='book_appointment'),
     path('confirmation/', views.appointment_confirmation, name='appointment_confirmation'),
+    path('patient/', views.patient_appointments, name='patient_appointments'),
+    path('specialist/', views.specialist_appointments, name='specialist_appointments'),
 ]
