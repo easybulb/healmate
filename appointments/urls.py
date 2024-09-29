bc0518e32db182c3bffccbc7e93437c72ed2b698
Henry Ofodieze <easybulb@gmail.com>
2024-09-29 05:46:42 +0000

    update appointments url.py to incluse path to cancel confirm page

--- appointments/urls.py
+++ appointments/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('patient/', views.patient_appointments, name='patient_appointments'),
     path('specialist/', views.specialist_appointments, name='specialist_appointments'),
     path('cancel/<int:appointment_id>/', views.cancel_appointment, name='cancel_appointment'),
-    path('cancel/<int:appointment_id>/', views.cancel_appointment, name='cancel_appointment'),
+    path('cancel/confirm/<int:appointment_id>/', views.confirm_cancel_appointment, name='confirm_cancel_appointment'),
 ]
