1b20227c5db5028913d3bb77d80da147f76bb97d
Henry Ofodieze <easybulb@gmail.com>
2024-09-30 06:04:15 +0000

    add views to book appointment in specialists views.py

--- specialists/views.py
+++ specialists/views.py
@@ -54,3 +54,23 @@ def specialist_detail(request, specialist_id):
         'availabilities': availabilities,
     })
 
+
+# Book Appointment View
+@login_required
+def book_appointment(request, specialist_id):
+    specialist = get_object_or_404(SpecialistProfile, id=specialist_id)
+    if request.method == 'POST':
+        form = AppointmentForm(request.POST)
+        if form.is_valid():
+            appointment = form.save(commit=False)
+            appointment.specialist = specialist
+            appointment.patient = request.user.patientprofile  # Assuming a patient profile is linked to the user
+            appointment.save()
+            return redirect('appointment_confirmation')  # Redirect to a confirmation page
+    else:
+        form = AppointmentForm()
+    
+    return render(request, 'appointments/book_appointment.html', {
+        'form': form,
+        'specialist': specialist
+    })
