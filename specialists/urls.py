ffca5e3692026bd08ab49a5527bba0be2c310c0c
Henry Ofodieze <easybulb@gmail.com>
2024-09-30 06:05:25 +0000

    add book_appointment path to specialists urls.py

--- specialists/urls.py
+++ specialists/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('search/', views.search_specialists, name='search_specialists'),
     path('detail/<int:specialist_id>/', views.specialist_detail, name='specialist_detail'),
+    path('book/<int:specialist_id>/', views.book_appointment, name='book_appointment'), 
 ]
