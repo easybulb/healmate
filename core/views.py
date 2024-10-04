38e7dab84d59968d0b5da9bfc65ef763e6ca939d
Henry Ofodieze <easybulb@gmail.com>
2024-10-04 13:22:41 +0000

    modify ContactPage class in core/views.py to handle post requests

--- core/views.py
+++ core/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from dashboard.models import Specialty
 from django.contrib.auth.models import Group
+from django.contrib import messages
+from .forms import ContactForm
+from .models import ContactMessage
+from django.views import View
 
 def home(request):
     specialties = Specialty.objects.all()
@@ -15,6 +19,18 @@ class AboutPage(TemplateView):
 
 class ContactPage(TemplateView):
     template_name = 'core/contact_us.html'
+
+    def get(self, request):
+        form = ContactForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your message has been sent successfully.")
+            return redirect('contact_us')  # Redirect to avoid resubmission on page refresh
+        return render(request, self.template_name, {'form': form})
 
 class JoinUsPage(TemplateView):
     template_name = 'core/join_us.html'
